2659d9bbf16cf30731f5f3a6b0ddbe2253a625b1
stevedevfrog <rodzikprzemyslaw@gmail.com>
2023-12-15 20:49:53 -0500

    Basic mechanics of the game in Javascript

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-let firstCard = 4;
-let secondCard = 6;
+let firstCard = 4
+let secondCard = 6
 
-sum = firstCard + secondCard;
-if (sum < 21)
+sum = firstCard + secondCard
+
+let hasBlackJack = false
+let isALive = true
+let message = ""
+
+if (sum < 21) message = "Do you want to draw a new card?"
+else if (sum === 21) {
+    message = "You won! You have a Blackjack! Congratulations!"
+    hasBlackJack = true
+} else {
+    message = "I'm sorry, You lost! Try again."
+    isALive = false
+}
+
+console.log(message)
