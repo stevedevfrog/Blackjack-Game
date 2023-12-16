c5b80b8c781d4351013adeec2b4cf43f21cf89b9
stevedevfrog <rodzikprzemyslaw@gmail.com>
2023-12-15 21:59:20 -0500

    Start Button and Draw Card button

--- index.js
+++ index.js
@@ -7,13 +7,35 @@ let hasBlackJack = false
 let isALive = true
 let message = ""
 
-if (sum < 21) message = "Do you want to draw a new card?"
-else if (sum === 21) {
-    message = "You won! You have a Blackjack! Congratulations!"
-    hasBlackJack = true
-} else {
-    message = "I'm sorry, You lost! Try again."
-    isALive = false
+function checkIfWin () {
+    if (sum < 21) message = "Do you want to draw a new card?"
+    else if (sum === 21) {
+        message = "You won! You have a Blackjack! Congratulations!"
+        hasBlackJack = true
+    } else {
+        message = "I'm sorry, You lost! Try again."
+        isALive = false
+    }
+
+    messageEl = document.getElementById("message-el")
+    messageEl.innerText = message
+
+    sumEl = document.getElementById("sum-el")
+    sumEl.innerText = sum
 }
 
-console.log(message)
+function drawCard () {
+    firstCard = Math.floor(Math.random() * 20) + 1;;
+    secondCard = Math.floor(Math.random() * 20) + 1;;
+    sum = firstCard + secondCard
+
+    cardsEl = document.getElementById("cards-el")
+    cardsEl.innerText = firstCard + " " + secondCard
+
+    checkIfWin()
+}
+
+function startGame () {
+    console.log("The game is started!")
+    drawCard()
+}
